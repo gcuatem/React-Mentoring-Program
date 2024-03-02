c197cab22b12c857132299e3c013857a2f70f656
gcm <gcuatem@yahoo.com.mx>
2024-03-02 13:46:26 -0600

    Updating tests for PR

--- module-1/src/Components/Counter/Counter.test.tsx
+++ module-1/src/Components/Counter/Counter.test.tsx
@@ -1,17 +1,24 @@
+import Counter from "./Counter";
 import { render,screen } from "@testing-library/react";
 import user from '@testing-library/user-event';
-import Counter from "./Counter";
+
 
 describe("Counter Component Tests",()=>{
-    //Test that component renders initial value provided in props
-    test("renders initial value provided in props",()=>{
-        const initialValue = 2; 
+
+    test("click event on decrement button decrements the displayed value",async ()=>{
+        let initialValue = 0; 
+        user.setup()
         render(<Counter initialValue={initialValue} />);
-        const valueElement = screen.getByText(`Counter: ${initialValue}`);
-        expect(valueElement).toBeInTheDocument();
-    });
-    
-    //Test that a click event on "decrement" button decrements the displayed value
+        const incrementButton = screen.getByRole('button',{name: "Increment"});
+        await user.click(incrementButton).then(
+            ()=>{
+                initialValue++;
+                const valueElement = screen.getByText(`Counter: ${initialValue}`);
+                expect(valueElement).toBeInTheDocument();
+            }
+        );
+    });    
+
     test("click event on decrement button decrements the displayed value",async ()=>{
         let initialValue = 1; 
         user.setup()
@@ -27,18 +34,12 @@ describe("Counter Component Tests",()=>{
         );
     });
 
-    //Test that a click event on "increment" button increments the displayed value
-    test("click event on decrement button decrements the displayed value",async ()=>{
-        let initialValue = 0; 
-        user.setup()
+
+    test("renders initial value provided in props",()=>{
+        const initialValue = 2; 
         render(<Counter initialValue={initialValue} />);
-        const incrementButton = screen.getByRole('button',{name: "Increment"});
-        await user.click(incrementButton).then(
-            ()=>{
-                initialValue++;
-                const valueElement = screen.getByText(`Counter: ${initialValue}`);
-                expect(valueElement).toBeInTheDocument();
-            }
-        );
+        const valueElement = screen.getByText(`Counter: ${initialValue}`);
+        expect(valueElement).toBeInTheDocument();
     });
+
 });
--- module-1/src/Components/Genres/Genres.test.tsx
+++ module-1/src/Components/Genres/Genres.test.tsx
@@ -1,44 +1,61 @@
-import { render,screen } from "@testing-library/react";
-import user from '@testing-library/user-event';
+//Genres test group
+import { render, screen } from "@testing-library/react";
+import user from "@testing-library/user-event";
 import Genres from "./Genres";
+describe("Genre Component tests", () => {
+  afterEach(() => {
+    vi.restoreAllMocks();
+  });
 
-describe.only("Genre Component tests",()=>{
+  test("after a click on a genre component calls callback with correct argument", async () => {
+    let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
+    const mockOnChange = vi.fn();
+    user.setup();
 
-    afterEach(() => {
-        vi.restoreAllMocks()
-      });
-
-    test("component renders all genres passed in props", async ()=>{
-        let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
-        const mockOnChange = vi.fn();
-        
-        render(<Genres genres={listOfGenres} selected={listOfGenres[1]} onSelect={mockOnChange}/>)
-        const buttonsDisplayed = await screen.findAllByRole('button');
-        expect(buttonsDisplayed).toHaveLength(listOfGenres.length);
+    render(
+      <Genres
+        genres={listOfGenres}
+        selected={listOfGenres[1]}
+        onSelect={mockOnChange}
+      />
+    );
+    const clickedButton = screen.getByRole("button", {
+      name: /drama/i,
     });
+    await user.click(clickedButton);
+    expect(mockOnChange).toHaveBeenCalledWith("Drama");
+  });
 
-    test("component highlights a selected genre passed in props",()=>{
-        let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
-        const mockOnChange = vi.fn();
-        
-        render(<Genres genres={listOfGenres} selected={listOfGenres[1]} onSelect={mockOnChange}/>)
-        const selectedButton=screen.getByRole('button', {
-            name: /horror/i
-          });
-        expect(selectedButton).toHaveClass("text-red-500");
-        expect(selectedButton).toHaveClass("border-green-600");
-    });
+  test("component highlights a selected genre passed in props", () => {
+    let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
+    const mockOnChange = vi.fn();
 
-    test("after a click on a genre component calls callback with correct argument", async ()=>{
-        let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
-        const mockOnChange = vi.fn();
-        user.setup()
-        
-        render(<Genres genres={listOfGenres} selected={listOfGenres[1]} onSelect={mockOnChange}/>);
-        const clickedButton=screen.getByRole('button', {
-            name: /drama/i
-          });
-        await user.click(clickedButton);
-        expect(mockOnChange).toHaveBeenCalledWith('Drama');
+    render(
+      <Genres
+        genres={listOfGenres}
+        selected={listOfGenres[1]}
+        onSelect={mockOnChange}
+      />
+    );
+    const selectedButton = screen.getByRole("button", {
+      name: /horror/i,
     });
+    expect(selectedButton).toHaveClass("text-red-500");
+    expect(selectedButton).toHaveClass("border-green-600");
+  });
+
+  test("component renders all genres passed in props", async () => {
+    let listOfGenres = ["Drama", "Horror", "Comedy", "Suspense", "Action"];
+    const mockOnChange = vi.fn();
+
+    render(
+      <Genres
+        genres={listOfGenres}
+        selected={listOfGenres[1]}
+        onSelect={mockOnChange}
+      />
+    );
+    const buttonsDisplayed = await screen.findAllByRole("button");
+    expect(buttonsDisplayed).toHaveLength(listOfGenres.length);
+  });
 });
