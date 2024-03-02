35e97bfaf5c0a52e6ed01b061e65d4552403f377
gcm <gcuatem@yahoo.com.mx>
2024-03-02 16:30:22 -0600

    Adding Cypress Test

--- module-1/src/Components/Counter/Counter.tsx
+++ module-1/src/Components/Counter/Counter.tsx
@@ -15,10 +15,10 @@ const Counter:React.FC<ICounterProps> = ({initialValue=0}) => {
       };
   return (
     <div className='border p-3 m-3'>
-      <h3 className='text-lg m-4 font-bold'>Counter Component</h3>
-       <p className='text-3xl m-2'>Counter: {count}</p>
-       <button className='ml-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 border border-blue-700 rounded' onClick={increment}>Increment</button>
-       <button className='ml-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 border border-blue-700 rounded' onClick={decrement}>Decrement</button>
+      <h3 data-testid="Cy-Counter-Title" className='text-lg m-4 font-bold'>Counter Component</h3>
+       <p data-testid="Cy-Counter-Text" className='text-3xl m-2'>Counter: {count}</p>
+       <button data-testid="Cy-Counter-Increment" className='ml-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 border border-blue-700 rounded' onClick={increment}>Increment</button>
+       <button data-testid="Cy-Counter-Decrement" className='ml-2 bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 border border-blue-700 rounded' onClick={decrement}>Decrement</button>
     </div>
   )
 }
